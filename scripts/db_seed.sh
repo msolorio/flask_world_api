5c4eb2e2c3ade4c11048522e116180b802a1c381
msolorio <msolorio.github@gmail.com>
2022-03-31 16:17:30 -0700

    Updates seed file with proper postgres endpoint

--- scripts/db_seed.sh
+++ scripts/db_seed.sh
@@ -1,4 +1,4 @@
 flask db upgrade
 
-psql ${SQLALCHEMY_DATABASE_URI} -f /app/seed/countries.sql
-psql ${SQLALCHEMY_DATABASE_URI} -f /app/seed/cities.sql
+psql ${DATABASE_URL} -f /app/seed/countries.sql
+psql ${DATABASE_URL} -f /app/seed/cities.sql
