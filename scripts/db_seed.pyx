5f423971e27f8a5bed047a38dd36ebe5090266d1
msolorio <msolorio.github@gmail.com>
2022-03-31 18:27:24 -0700

    Updates seed script

--- scripts/db_seed.pyx
+++ scripts/db_seed.pyx
@@ -1,6 +1,5 @@
 import os
 DATABASE_URL = os.environ['DATABASE_URL']
-# UPDATED_DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 
 os.system('flask db upgrade')
 os.system(f'psql {DATABASE_URL} -f /app/seed/countries.sql')
